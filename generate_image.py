02360c594486ad7045eaa289c268827ca1e064e0
Pol Alvarez <pol.avms@gmail.com>
2023-06-03 12:03:34 +0200

    v2

--- generate_image.py
+++ generate_image.py
@@ -1,35 +1,55 @@
-# Python script that generates images of Chinese characters with their pronunciation and translation using the Google Translate API and the Pillow library:
-# This script uses the `googletrans` library to translate Chinese characters to English and get their pronunciation. It also uses the `Pillow` library to generate images with the character, its translation, and pronunciation. You can modify this script to use a different list of characters or change the layout of the generated images.
-
-# Please note that you'll need to install the `googletrans` and `Pillow` libraries by running `pip install googletrans Pillow` before running this script.
-
-from googletrans import Translator
 from PIL import Image, ImageDraw, ImageFont
-
-# Set up the translator
-translator = Translator()
+import pandas as pd
+import textwrap
+import os
 
 # Set up the font
-#font = ImageFont.truetype('Arial.ttf', 36)
-font = ImageFont.truetype('NotoSansTC-Regular.otf', 36)
+font_path = 'NotoSansTC-Regular.otf'
+base_font_size = 36
+# Dimensions of the image
+img_width = 800
+img_height = 600
 
+# Read the CSV file
+df = pd.read_csv('hanziDB.csv')
 
-# List of Chinese characters
-characters = ['你', '好', '吗']
+# Create images directory if it doesn't exist
+os.makedirs('img', exist_ok=True)
 
-for character in characters:
-    # Get the translation and pronunciation
-    translation = translator.translate(character, src='zh-cn', dest='en')
-    pronunciation = translation.extra_data['origin_pronunciation']
+# Function to adjust font size based on text length
+def adjust_font_size(draw, text, font_path, base_font_size, max_width):
+    font_size = base_font_size
+    font = ImageFont.truetype(font_path, font_size)
+    w, h = draw.textsize(text, font=font)
+    while w > max_width:
+        font_size -= 1
+        font = ImageFont.truetype(font_path, font_size)
+        w, h = draw.textsize(text, font=font)
+    return font
+
+for index, row in df.iterrows():
+    character = row['character']
+    pronunciation = row['pinyin']
+    definition = textwrap.fill(row['definition'], width=30)  # wrap the text to fit into image
 
     # Create a new image
-    img = Image.new('RGB', (200, 200), color='white')
+    img = Image.new('RGB', (img_width, img_height), color='white')
     d = ImageDraw.Draw(img)
 
     # Add the text to the image
-    d.text((10, 10), character, font=font, fill='black')
-    d.text((10, 60), translation.text, font=font, fill='black')
-    d.text((10, 110), pronunciation, font=font, fill='black')
+    char_font = ImageFont.truetype(font_path, base_font_size)
+    d.text((10, 10), character, font=char_font, fill='black')
+    _, char_height = d.textsize(character, font=char_font)
+
+    def_font = adjust_font_size(d, definition, font_path, base_font_size, img.width - 20)
+    d.text((10, char_height + 20), definition, font=def_font, fill='black')
+    _, def_height = d.textsize(definition, font=def_font)
+
+    pron_font = ImageFont.truetype(font_path, base_font_size)
+    d.text((10, char_height + def_height + 30), pronunciation, font=pron_font, fill='black')
 
     # Save the image
-    img.save(f'{pronunciation}.png')
+    img.save(f'img/{pronunciation}.png')
+
+    # Stop after generating one image
+    break
